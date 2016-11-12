ba7878bf8471e64af1247c4a9e3915043088a549
pramayu <gedepramayu@gmail.com>
2016-11-12 20:21:57 +0700

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,28 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'khapoo'
+
 set :repo_url, 'git@github.com:pramayu/booom.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :user, 'pramayu'
+set :application, 'khapoo'
+set :rails_env, 'production'
+server '103.247.8.220', user: "#{fetch(:user)}", roles: %w{app web}, primary: true
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :pty, true
+set :rbenv_ruby, '2.3.1'
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/pramayu/khapoo_app'
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# Default value for :scm is :git
-# set :scm, :git
+set :config_example_suffix, '.example'
+set :config_files, %w{config/database.yml config/secrets.yml}
+set :puma_conf, "#{shared_path}/config/puma.rb"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
+  before 'check:linked_files', 'config:push'
+  before 'check:linked_files', 'puma:config'
+  before 'check:linked_files', 'puma:nginx_config'
+  before 'deploy:migrate', 'deploy:db:create'
+  after 'puma:smart_restart', 'nginx:restart'
 end
